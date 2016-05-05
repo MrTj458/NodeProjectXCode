d9dfcb4b97697a5ee9e0ac18cef36a8d81b80bb3
Trevor <thod0127@740s-pl-130.local>
2016-05-05 12:25:42 -0600

    Added the constructor to the CTECHashTable cpp

--- Model/CTECHashTable.cpp
+++ Model/CTECHashTable.cpp
@@ -7,3 +7,24 @@
 //
 
 #include "CTECHashTable.h"
+#include <iostream>
+using namespace std;
+
+/*
+ * Constructor for the CTECHashTable. Initialzes all data members.
+ * Sets the capacity of the base storage to 101.
+ */
+template <class Type>
+CTECHashTable<Type> :: CTECHashTable()
+{
+    this->size = 0;
+    this->capacity = 101;
+    this->efficiencyPercentage = 0.667;
+    this->internalStorage = new Type[capacity];
+}
+
+template <class Type>
+CTECHashTable<Type> :: ~CTECHashTable<Type>()
+{
+    
+}
