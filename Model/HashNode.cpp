0ab207fc54eb246a175685b678fa6eb1fcc6d3e1
Trevor <thod0127@740s-pl-130.local>
2016-05-09 11:58:24 -0600

    Added HashNode class

---
+++ Model/HashNode.cpp
@@ -0,0 +1,9 @@
+//
+//  HashNode.cpp
+//  NodeProjectXCode
+//
+//  Created by Hodsdon, Trevor on 5/9/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "HashNode.h"
---
+++ Model/HashNode.h
@@ -0,0 +1,27 @@
+//
+//  HashNode.hpp
+//  NodeProjectXCode
+//
+//  Created by Hodsdon, Trevor on 5/9/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef HashNode_hpp
+#define HashNode_hpp
+
+#include <stdio.h>
+
+template <class Type>
+class HashNode
+{
+private:
+    int key;
+    Type value;
+    
+public:
+    HashNode(int key, const Type& value);
+    int getKey();
+    Type getValue();
+};
+
+#endif /* HashNode_hpp */
